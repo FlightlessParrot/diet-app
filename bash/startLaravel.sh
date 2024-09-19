b8bffae3eb9b53c754efbf9be6825457164dd246
Konrad <shrimpinweb@gmail.com>
2024-09-20 00:21:30 +0200

    bash changes

--- bash/startLaravel.sh
+++ bash/startLaravel.sh
@@ -7,6 +7,8 @@ read stop;
 if [ "$stop" == 'true' -o "$stop" == '1' -o "$stop" = "" ];
 then bash ./bash/stopServices.sh;
 fi;
+sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0;
+systemctl --user start docker-desktop;
 echo -e "${RED}I am calling on your servants, my Shrimp.${NC}"
 echo 'starting sail';
 ./vendor/bin/sail up -d;
